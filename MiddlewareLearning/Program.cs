d2942df71f9a35546a36f1cae1461bb2f40c06e0
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2024-02-21 14:12:23 -0300

    aula de middleware chain

--- MiddlewareLearning/Program.cs
+++ MiddlewareLearning/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+//1° chamada
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    //lógica
+    await next(context);
+});
+
+//2° chamada
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    //logica
+    await next(context);
+});
+
+//3° chamada
 app.Run(async (HttpContext context) =>
 {
     await context.Response.WriteAsync("Hello");
